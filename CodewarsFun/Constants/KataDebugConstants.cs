c19cc211692d7074506126eea1033719f9641e63
apAbsoluteZero <hsF77S6y5BwzVrSzntdJ>
2023-09-04 00:47:09 +0300

    fix: orthography mistake

--- CodewarsFun/Constants/KataDebugConstants.cs
+++ CodewarsFun/Constants/KataDebugConstants.cs
@@ -8,7 +8,7 @@ public static class KataDebugConstants
     public const string TESTING_TEST_NUMBER = "Тестирование #";
     public const string TESTING_PASSED = "(Пройдено)";
     public const string TESTING_FAILED = "(Ошибка)";
-    public const string TESTING_WITH_ARGS = "с агруиентами";
+    public const string TESTING_WITH_ARGS = "с агрументами";
     public const string TESTING_SUCCESS_RESULT = "--> Тестирование успешно завершено!";
     public const string TESTING_FAILED_RESULT = "--> Тестирование завершено с ошибками!";
     public const string TESTING_APPROVE_TO_GLOBAL_TEST = "--> Можно отправлять на проверку!";
